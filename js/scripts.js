f10755b31396bd47a7f1859b7b9681b5805966d6
johnwanjema <jonwanjema@gmail.com>
2019-05-10 14:08:32 +0300

    shows rolled number

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,24 @@
-function roll1() {
-    var x = document.getElementById("roll1")
-    x.innerHTML = Math.floor((Math.random() * 6) + 1);
-}
-function roll2() {
-    var x = document.getElementById("roll2")
-    x.innerHTML = Math.floor((Math.random() * 6) + 1);
+function roll() {
+    var x = Math.floor((Math.random() * 6) + 1);
+    return x;
 }
 
 
-$(document).ready(function() {    
-   var player1 = prompt("input name of first player")
-   var player2 = prompt("input name of second player")
-   document.getElementById("player1").innerHTML =
-   "Hello " + player1 + "! How are you today?";
-   document.getElementById("player2").innerHTML =
-   "Hello " + player2 + "! How are you today?";
+$(document).ready(function () {
+    $("#rol").click(function () {
+        $("#roll1").text(roll());
+    });
+    $("#roll").click(function () {
+        $("#roll2").text(roll());
+    });
+
+
+
+
+
+    var player1 = prompt("input name of first player")
+    var player2 = prompt("input name of second player")
+    document.getElementById("player1").innerHTML = player1
+
+    document.getElementById("player2").innerHTML = player2
 });

029d718df9a0c32119951079cd4bd1716966eea0
johnwanjema <jonwanjema@gmail.com>
2019-05-10 11:30:17 +0300

    added fucction to generate a random number

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function roll1() {
+    var x = document.getElementById("roll1")
+    x.innerHTML = Math.floor((Math.random() * 6) + 1);
+}
+function roll2() {
+    var x = document.getElementById("roll2")
+    x.innerHTML = Math.floor((Math.random() * 6) + 1);
+}

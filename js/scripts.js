f9d161c9e323f36db90fffbc9702042028d0e62a
johnwanjema <jonwanjema@gmail.com>
2019-05-11 15:46:10 +0300

    added construct for player

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,39 @@
 function roll() {
-    var x = Math.floor((Math.random() * 6) + 1);
-    return x;
+    var randomNumber = Math.floor((Math.random() * 6) + 1);
+
+    return randomNumber
+
+};
+
+
+function player (name, score) {
+  this.name = name;
+  this.score = score;  
 }
 
 
 $(document).ready(function () {
     $("#rol").click(function () {
+
         $("#roll1").text(roll());
+
+
     });
     $("#roll").click(function () {
         $("#roll2").text(roll());
     });
+    var player1 = new player (prompt("input name of first player"));
+    var player2 = new player (prompt("input name of second player"));
+
+    $("#player1").text(player1);
+    $("#player2").text(player2);
+    $("#total1").text();
 
 
 
-
-
-    var player1 = prompt("input name of first player")
-    var player2 = prompt("input name of second player")
-    document.getElementById("player1").innerHTML = player1
-
-    document.getElementById("player2").innerHTML = player2
 });
+
+
+
+
+

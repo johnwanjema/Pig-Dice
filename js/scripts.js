b7c5ded32ad5c9abd5356c23221f45eef0bb29a1
johnwanjema <jonwanjema@gmail.com>
2019-05-12 10:59:32 +0300

    added a prototype dice roller

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,84 @@
-function roll() {
-    var randomNumber = Math.floor((Math.random() * 6) + 1);
+function Player(userName) {
+  this.userName = userName;
+  
+};
 
-    return randomNumber
+//pass it a player object!
+function Turn(player) {
+  this.total = 0;
+  this.randNumber = 0;
+  this.player = player;
+};
 
+//Creates random number, saves it and then returns it
+Turn.prototype.diceRoller = function (player1, player2) {
+  var randNumber = Math.floor(Math.random() * 6) + 1;
+  this.total += randNumber;
+  
+  if (randNumber == 1) {
+    this.total = 0;
+    
+    this.endTurn(player1, player2);
+    // this.randNumber += randNumber;
+    return randNumber;
+  } else {
+    
+    
+    return randNumber;
+
+  };
+};
+
+Turn.prototype.endTurn = function (player1, player2) {
+  //adding total to score
+  
+  //and clearing total
+  this.total = 0;
+  this.randNumber = 0;
+  if (this.player == player1) {
+    this.player = player2;
+    $("#player2").toggleClass("active");
+    $("#player1").toggleClass("active");
+  } else if (this.player == player2) {
+    this.player = player1;
+    $("#player2").toggleClass("active");
+    $("#player1").toggleClass("active");
+  };
 };
 
 
-function player (name, score) {
-  this.name = name;
-  this.score = score;  
-}
+
+
+
+
+
 
 
 $(document).ready(function () {
-    $("#rol").click(function () {
+  $("#rol").click(function () {
+    $("#roll1").text(playing.diceRoller());
+    $("#total1").text(playing.total);
+    
+ 
 
-        $("#roll1").text(roll());
+  });
+  $("#roll").click(function () {
+    $("#roll2").text(playing.diceRoller());
+    $("#total2").text(playing.total);
+    $('#t1').text(player2.score);
+ 
 
+  });
+  
+  var player1 = new Player(prompt("input name"));
+  var player2 = new Player(prompt("input name"));
+  var playing = new Turn(player1);
 
-    });
-    $("#roll").click(function () {
-        $("#roll2").text(roll());
-    });
-    var player1 = new player (prompt("input name of first player"));
-    var player2 = new player (prompt("input name of second player"));
+  
+  $("#player1").text(player1.userName);
+  $("#player2").text(player2.userName);
 
-    $("#player1").text(player1);
-    $("#player2").text(player2);
-    $("#total1").text();
+  
 
 
 
